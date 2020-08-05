7dc4dd4f734cb5ab9023ed819abaf9fe747c4a1a
NeuroscientistAlzheimer <tatarinov.ivan15.10.19@yandex.ru>
2020-08-05 20:20:25 +0300

    Improvements

--- app/src/main/java/com/example/newconverter/MainActivity.java
+++ app/src/main/java/com/example/newconverter/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.newconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
     SpinnerDialog spinnerCurrency2;
     Button currency1;
     Button currency2;
+    Button button_res;
+    EditText num;
+    float numCurrensy1;
+    float numCurrensy2;
+    float calculationCurrensy;
+    float number;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         mTextViewResult = findViewById(R.id.text_view_result);
         mQueue = Volley.newRequestQueue(this);
         Log.d("Pars","Начало");
-        jsonParse();
         Log.d("Pars","Конец");
         final ArrayList<String> itemsCurrency = new ArrayList<>();
         itemsCurrency.add("USD");
@@ -51,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         itemsCurrency.add("AUD");
         currency1 = findViewById(R.id.currency1);
         currency2 = findViewById(R.id.currency2);
+        button_res = findViewById(R.id.button_res);
+        num = findViewById(R.id.num);
 
         spinnerCurrency1 = new SpinnerDialog(MainActivity.this,itemsCurrency,"Select Currency");
         spinnerCurrency1.setCancellable(true);
@@ -86,19 +95,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        button_res.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("Pars","кнопка");
+                String first;
+                String one;
+                one = (String) currency2.getText();
+                first =(String) currency1.getText();
+                Log.d("Pars","Начало" + one + first);
+                jsonParse(first,one);
+            }
+        });
 
     }
-    private void jsonParse(){
+
+
+    private void jsonParse(final String a, final String b){
         Log.d("Pars","Вход");
-        String url = "http://api.currencylayer.com/live?access_key=ad16a4ab373cbeca1bdbf0fd5b1e77ed&%20currencies=USD,AUD,CAD,PLN,MXN,RUB&format=1";
+        String url = "http://api.currencylayer.com/live?access_key=ad16a4ab373cbeca1bdbf0fd5b1e77ed&%20currencies="+a+","+b+"&format=1";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             public void onResponse(JSONObject response) {
                 try {
                     Log.d("Pars", "присвоение");
                     JSONObject last = (JSONObject) response.get("quotes");
-                    String rub = last.getString("USDRUB");
-                    mTextViewResult.append(String.valueOf(rub));
+                    String nameCurrensy1 = "USD" + b;
+                    String nameCurrensy2 = "USD" + a;
+                    float res;
+                    String numS = num.getText().toString();
+                    float numC =Float.parseFloat(numS);
+                    numCurrensy1 = (float) last.getDouble(nameCurrensy1);
+                    numCurrensy2 = (float) last.getDouble(nameCurrensy2);
+                    calculationCurrensy = numCurrensy1 / numCurrensy2;
+                    res = calculationCurrensy * numC;
+                    String resS = String.format("%.2f",res);
+                    Log.d("Pars", "присвоение"+ numC +"\n"+numCurrensy1+"\n"+numCurrensy2 + "\n" + calculationCurrensy + "\n" + resS);
+                    mTextViewResult.setText(resS);
                     Log.d("Pars", "завершение" );
                 } catch (JSONException e) {
                     e.printStackTrace();
